a5ca1e778b8cfbc98b1b7f04eb3dad007c1f01a4
iobox420 <iobox420@gmail.com>
2023-02-20 16:44:26 +0500

    feat: commit где есть мой слайдер

--- pages/test6.vue
+++ pages/test6.vue
@@ -11,13 +11,16 @@
             :value="tStore.rangeValue"
             :range="true"
             />
+        <Slider v-model="values" />
     </div>
 </template>
 
 <script setup>
+import Slider from "@vueform/slider"
 import { computed, ref } from "vue"
 import testStore from "../store/testStore.js"
 const tStore = testStore()
+const values = ref([10,199])
 const value = ref([0, 10])
 const computedVal = computed({
     get() {
---
+++ pages/test7.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="p100">
+        Hello world
+        <Slider v-model="value" />
+    </div>
+</template>
+<script>
+import Slider from "@vueform/slider"
+import { defineComponent } from "vue"
+import { Swiper, SwiperSlide } from "swiper/vue"
+// import 'swiper/css';
+
+export default defineComponent({
+    components: {
+        Slider,
+        Swiper,
+        SwiperSlide
+    },
+    setup() {
+        const onSwiper = (swiper) => {
+            console.log(swiper)
+        }
+        const onSlideChange = () => {
+            console.log("slide change")
+        }
+
+        return {
+            onSwiper,
+            onSlideChange
+        }
+    },
+    data() {
+        return {
+            value: 20
+        }
+    }
+})
+</script>
+
+<style src="@vueform/slider/themes/default.css"></style>
