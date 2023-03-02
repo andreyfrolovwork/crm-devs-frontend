3ab3e5bc725571225c2c032a4f9864615a1c3d77
iobox420 <iobox420@gmail.com>
2023-03-02 10:32:23 +0500

    feat: gallery advantages

--- components/the-gallery-advantages.vue
+++ components/the-gallery-advantages.vue
@@ -1,11 +1,7 @@
 <template>
-    <div class="mwr">
-        <div class="mwr-content">
-            <the-index-top />
-            <the-gallery-advantages/>
-            <div
-                v-animate-on-scroll
-                >
+    <splide :options="options">
+        <splide-slide>
+            <div class="splide-image-wrapper">
                 <the-index-block
                     h1="Заголовок"
                     image="index-4.png"
@@ -23,13 +19,13 @@
                     </template>
                 </the-index-block>
             </div>
-            <div
-                v-animate-on-scroll
-                >
+        </splide-slide>
+        <splide-slide>
+            <div class="splide-image-wrapper">
                 <the-index-block
                     h1="Заголовок"
                     image="index-3.png"
-                    reverse="true"
+
                     >
                     <template #h1text>
                         Парадные
@@ -41,9 +37,9 @@
                     </template>
                 </the-index-block>
             </div>
-            <div
-                v-animate-on-scroll
-                >
+        </splide-slide>
+        <splide-slide>
+            <div class="splide-image-wrapper">
                 <the-index-block
                     h1="Заголовок"
                     image="chrome_2.png"
@@ -58,13 +54,30 @@
                     </template>
                 </the-index-block>
             </div>
-            <div
-                v-animate-on-scroll
-                >
+        </splide-slide>
+        <splide-slide>
+            <div class="splide-image-wrapper">
+                <the-index-block
+                    h1="Заголовок"
+                    image="chrome_2.png"
+                    >
+                    <template #h1text>
+                        Комфортные планировки
+                    </template>
+                    <template #text>
+                        В "Доме на Кирова" большое количество функциональных планировок, которые прорабатывались с
+                        учетом современных потребностей: от компактных студий до квартир для больших семей — с
+                        кухней-гостиной, несколькими спальнями и двумя санузлами.
+                    </template>
+                </the-index-block>
+            </div>
+        </splide-slide>
+        <splide-slide>
+            <div class="splide-image-wrapper">
                 <the-index-block
                     h1="Заголовок"
                     image="index1.png"
-                    reverse="true"
+
                     >
                     <template #h1text>
                         Закрытая территория и двор без машин
@@ -76,9 +89,9 @@
                     </template>
                 </the-index-block>
             </div>
-            <div
-                v-animate-on-scroll
-                >
+        </splide-slide>
+        <splide-slide>
+            <div class="splide-image-wrapper">
                 <the-index-block
                     h1="Заголовок"
                     image="index1.png"
@@ -93,13 +106,13 @@
                     </template>
                 </the-index-block>
             </div>
-            <div
-                v-animate-on-scroll
-                >
+        </splide-slide>
+        <splide-slide>
+            <div class="splide-image-wrapper">
                 <the-index-block
                     h1="Заголовок"
                     image="index-2.png"
-                    reverse="true"
+
                     >
                     <template #h1text>
                         Горнолыжный курорт и озеро Тургояк
@@ -111,9 +124,9 @@
                     </template>
                 </the-index-block>
             </div>
-            <div
-                v-animate-on-scroll
-                >
+        </splide-slide>
+        <splide-slide>
+            <div class="splide-image-wrapper">
                 <the-index-block
                     h1="Заголовок"
                     image="index1.png"
@@ -128,86 +141,34 @@
                     </template>
                 </the-index-block>
             </div>
-            <div
-                v-animate-on-scroll
-                >
-                <div class="howbuy m0auto1440 p100">
-                    <div class="h1 howbuy_title">
-                        Как купить?
-                    </div>
-                    <div class="howbuy-text">
-                        Продажи в ведутся строго по договорам долевого участия (ДДУ),
-                        согласно действующему законодательству РФ (№ 214‑ФЗ)
-                    </div>
-                    <the-tabs :tabs="['Ипотека','Рассрочка'/*,'Материнский капитал','100% оплата','Trade-in'*/]">
-                        <template #t0>
-                            <the-credit />
-                        </template>
-                        <template #t1>
-                            Текст про рассрочку
-                        </template>
-                        <!--                <template #t2>
-                                            t2
-                                        </template>
-                        
-                                        <template #t3>
-                                            t3
-                                        </template>
-                                        <template #t4>
-                                            t4
-                                        </template>
-                                        <template #t5>
-                                            t5
-                                        </template>-->
-                    </the-tabs>
-                </div>
-            </div>
-            <div
-
-                class="mapblock p100"
-                >
-                <div class="mapblock-child m0auto">
-                    <MapDetailYandex
-                        :card-point="[55.044555,60.082794]"
-                        :card-point-hint="'Дом на кирова'"
-                        :card-point-balloon="'Дом на кирова'"
-                        />
-                </div>
-            </div>
-        </div>
-    </div>
+        </splide-slide>
+    </splide>
 </template>
 
 <script setup>
 import { useRuntimeConfig } from "nuxt/app"
 
+const options = {
+    rewind: true,
+    gap: "1rem",
+    width: "100%",
+    height: "100%"
+}
 const config = useRuntimeConfig()
-
-
 </script>
 
-<style lang="scss">
-.mwr {
-    background-color: $background-color;
-    padding-top: 60px;
-    padding-bottom: 100px;
-    height: 100vh;
-    overflow: scroll;
-
-    &-content {
-        display: flex;
-        flex-flow: column nowrap;
-        justify-content: center;
-        align-content: center;
-    }
+<style scoped>
+.gallery-item {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
 }
 
-.mapblock {
-    /*    display: flex;
-        justify-content: center;*/
-    &-child {
-        max-width: 1440px;
-        margin: 0 auto;
-    }
+.splide-image-wrapper {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
 }
 </style>
