a9a7418c617e32ea97f70db94d1c83431918bdaf
iobox420 <iobox420@gmail.com>
2023-03-03 15:13:45 +0500

    feat: delete slider

--- plugins/slider.js
+++
@@ -1,7 +0,0 @@
-import { defineNuxtPlugin } from "#app"
-import "@vueform/slider/themes/default.css"
-import Slider from "@vueform/slider"
-
-export default defineNuxtPlugin((nuxtApp) => {
-    nuxtApp.vueApp.use(Slider)
-})
